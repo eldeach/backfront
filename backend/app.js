62bdc3e46e711c58381a7b33efa7a6492466df47
이돈형 <dhm_w@icloud.com>
2024-12-08 21:58:15 +0900

    github actinos

--- backend/app.js
+++ backend/app.js
@@ -37,10 +37,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api', require('./routes/api'));
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
