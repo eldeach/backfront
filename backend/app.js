4985126563fd1bc9653ce3bc77aef75a60312484
이돈형 <dhm_w@icloud.com>
2024-12-10 21:28:52 +0900

    edit

--- backend/app.js
+++ backend/app.js
@@ -24,6 +24,7 @@ const options = {
 const openapiSpec = swaggerJsdoc(options);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapiSpec));
 
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
